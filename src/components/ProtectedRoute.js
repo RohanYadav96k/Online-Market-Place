aa54649020b048f79e37c586a949054a53eae67f
Rohan Yadav <rohanyadav96k@gmail.com>
2025-08-02 15:16:39 +0530

    old code removed ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -13,9 +13,7 @@ const ProtectedRoute = ({ children }) => {
   }
   
   if (!user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to. This allows us to send them along to that page after they
-    // log in, which is a nicer user experience.
+    
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
